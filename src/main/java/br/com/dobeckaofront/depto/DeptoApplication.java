464a892cd456ac69e281a7180d0f1120a8af1b64
WILSON <WILSON@host.docker.internal>
2024-05-28 22:11:00 -0300

    Commit Initial

---
+++ src/main/java/br/com/dobeckaofront/depto/DeptoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dobeckaofront.depto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptoApplication.class, args);
+	}
+
+}
